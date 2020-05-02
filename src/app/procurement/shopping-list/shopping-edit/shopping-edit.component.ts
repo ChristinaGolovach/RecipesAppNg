964c9c5b914aef04708a3042d805532bfb2d03c8
Christina Golovach <christina.golovach@gmail.com>
2020-05-02 17:07:26 +0300

    add new Ingredient using ViewChild aproach

--- src/app/procurement/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/procurement/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter, Output, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameElement: ElementRef<HTMLInputElement>;
+  @ViewChild('amountInput') amountElement: ElementRef<HTMLInputElement>;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddIngredient(): void {
+    const name = this.nameElement.nativeElement.value;
+    const amount = +this.amountElement.nativeElement.value;
+
+    const newIngredient =  new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
